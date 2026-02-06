7becc7e0d8ec5c6eb22f8045146bfb23ef474af3
Selva <imselva1512@gmail.com>
2026-02-06 11:38:00 +0530

    Scroll view added

--- js/main.js
+++ js/main.js
@@ -786,3 +786,14 @@ flatpickr("#startDate", {
 flatpickr("#endDate", {
     dateFormat: "d/m/Y"
 });
+
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
